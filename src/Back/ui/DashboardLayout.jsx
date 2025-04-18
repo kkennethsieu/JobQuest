2546a0c5705ff8cdd82b85e6ae0a0785d880a4e1
kkennethsieu <kkennethsieu@gmail.com>
2025-04-18 14:25:11 -0700

    updated bug

--- src/Back/ui/DashboardLayout.jsx
+++ src/Back/ui/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import Sidebar from "./Sidebar/Sidebar";
+import Sidebar from "./SideBar/Sidebar";
 
 function DashboardLayout() {
   return (
